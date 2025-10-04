2c0c2bcd8e5a77a633e3433a49f39a71e2f8e9d2
Tokoni <108711341+Osclamao@users.noreply.github.com>
2025-10-05 00:09:47 +0100

    Add files via upload

---
+++ server.php
@@ -0,0 +1,46 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class NotificationServer implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            $client->send($msg);
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+}
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new NotificationServer()
+        )
+    ),
+    8080 // Port
+);
+echo "WebSocket server running on ws://localhost:8080\n";
+$server->run();
